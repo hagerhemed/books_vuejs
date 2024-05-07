9440fd0b97426dd9da80446687c38e9193b05fd1
Hager Gaber <108762778+hagerhemed@users.noreply.github.com>
2024-05-07 18:01:42 +0300

    Add files via upload

---
+++ day1/books.js
@@ -0,0 +1,66 @@
+const books = [
+    {
+      name: "The Great Gatsby",
+      category: "Fiction",
+      image: "https://i.pinimg.com/236x/07/5f/45/075f45438edf04dacfd5210b277fafbc.jpg",
+      pages: 44,
+      author: "F. Scott Fitzgerald",
+      price: 12.99
+    },
+    {
+      name: "The Catcher Rye",
+      category: "Non-Fiction",
+      image: "https://c8.alamy.com/comp/P9CE9W/book-cover-of-the-catcher-in-the-rye-by-j-d-salinger-first-edition-museum-private-collection-P9CE9W.jpg",
+      pages: 25,
+      author: "Yuval Noah Harari",
+      price: 17.99
+    },
+    {
+      name: "To Kill a Mockingbird",
+      category: "Fiction",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg/220px-To_Kill_a_Mockingbird_%281963_US_theatrical_poster%29.jpg",
+      pages: 281,
+      author: "Harper Lee",
+      price: 10.99
+    },
+    {
+      name: "The Chronicles of Narnia",
+      category: "Non-Fiction",
+      image: "https://c8.alamy.com/comp/2G7GBMH/the-chronicles-of-narnia-the-lion-the-witch-and-the-wardrobe-2005-directed-by-andrew-adamson-and-starring-tilda-swinton-georgie-henley-william-moseley-and-james-mcavoy-big-screen-adaptation-of-cs-lewis-classic-story-about-4-children-who-travel-through-a-wardrobe-to-the-magical-land-of-narnia-2G7GBMH.jpg",
+      pages: 443,
+      author: "Yuval Noah Harari",
+      price: 17.99
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      category: "Fiction",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3hRuZ5URsL3OXlebETqOOwyr8E73tdZMo7E-bfj2dw&s",
+      pages: 48,
+      author: "F. Scott Fitzgerald",
+      price: 12.99
+    },
+    {
+      name: "To Kill a Kingdom",
+      category: "Fiction",
+      image: "https://m.media-amazon.com/images/I/41yK4jhgekL._AC_.jpg",
+      pages: 281,
+      author: "Harper Lee",
+      price: 10.99
+    },
+    {
+      name: "The Great Gatsby",
+      category: "Fiction",
+      image: "https://i.pinimg.com/236x/07/5f/45/075f45438edf04dacfd5210b277fafbc.jpg",
+      pages: 44,
+      author: "F. Scott Fitzgerald",
+      price: 12.99
+    },
+    {
+      name: "The Catcher Rye",
+      category: "Non-Fiction",
+      image: "https://c8.alamy.com/comp/P9CE9W/book-cover-of-the-catcher-in-the-rye-by-j-d-salinger-first-edition-museum-private-collection-P9CE9W.jpg",
+      pages: 443,
+      author: "Yuval Noah Harari",
+      price: 17.99
+    },
+  ];
